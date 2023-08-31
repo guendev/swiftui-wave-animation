9e11a86df2947f79b7e9190020fe74784c869c2d
Guen <heloo@guen.dev>
2023-08-31 13:01:30 +0700

    Initial Commit

---
+++ wave/waveApp.swift
@@ -0,0 +1,17 @@
+//
+//  waveApp.swift
+//  wave
+//
+//  Created by Guen on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct waveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
